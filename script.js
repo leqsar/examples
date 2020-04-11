65f932bbbafec3aac611187a38dc8b6fe9d75132
unknown <arteg132579@gmail.com>
2020-04-11 17:01:01 +0300

    feat: add representation of mode on the switch ekement

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 window.onload = () => {
-    const switchElemnt = document.querySelector('.switch-element'),
+    const switchElement = document.querySelector('.switch-element'),
         slider = document.querySelector('.slider'),
         allCards = document.querySelectorAll('.card');
-    let mode = 'train';
+    let mode = 'train',
+        nameOfMode = document.createElement('P');
+    nameOfMode.textContent = `${mode.toUpperCase()}`;
+    nameOfMode.classList.add('switch-element-text');
+    switchElement.prepend(nameOfMode);
+
     slider.addEventListener('click', () => {
         mode = mode == 'train' ? 'game' : 'train';
+        nameOfMode.textContent = `${mode.toUpperCase()}`;
         if (mode == 'train') {
             slider.style.marginLeft = '2px';
-            switchElemnt.classList.remove('switch-element-game-mode');
+            switchElement.classList.remove('switch-element-game-mode');
+            nameOfMode.classList.remove('switch-text-game-mode');
             allCards.forEach(item => {
                 item.classList.remove('card-game-mode');
             });
         } else {
             slider.style.marginLeft = '68px';
-            switchElemnt.classList.add('switch-element-game-mode');
+            switchElement.classList.add('switch-element-game-mode');
+            nameOfMode.classList.add('switch-text-game-mode');
             allCards.forEach(item => {
                 item.classList.add('card-game-mode');
             });
         }
-    })
+    });
+
 }
