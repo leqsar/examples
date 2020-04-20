11ed1e4216ae2de2d77a38a56586d4fb3e113513
leqsar <47753538+leqsar@users.noreply.github.com>
2020-04-20 19:33:46 +0300

    feat: add posibility to close burger menu by click on icon

--- burger-menu.js
+++ burger-menu.js
@@ -5,24 +5,22 @@ export default function burgerMenuOpen() {
     let isMenuOpen = false;
 
     menuButton.addEventListener('click', () => {
-        isMenuOpen = isMenuOpen == true ? false : true;
+        isMenuOpen = isMenuOpen ? false : true;
         if (isMenuOpen) {
             menu.style.left = '0vw';
             burgerIconWrap.style.transform = 'rotate(90deg)';
-        } else {
-            menu.style.left = '-25vw';
-            burgerIconWrap.style.transform = 'rotate(0deg)';
         }
     });
-    
-    ////////// ТУТ ОШИБКА !!!!!!!!!!!!!!!!
+
     document.addEventListener('click', event => {
         let isMenu =  event.target.classList.contains('burger-menu-wrap'),
             isMenuIcon = event.target.classList.contains('burger-menu');
         if ((isMenuOpen && !isMenu) || isMenuIcon) {
             menu.style.left = '-25vw';
             burgerIconWrap.style.transform = 'rotate(0deg)';
-            isMenuOpen = false;
+            setTimeout(() => {
+                isMenuOpen = false;
+            }, 200);
         }
     }, true);
 }
