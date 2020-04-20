c5dc3e93dc5001be0ff16ec305e64f5da15e1b11
leqsar <47753538+leqsar@users.noreply.github.com>
2020-04-20 20:19:53 +0300

    refactor: change structure of the project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './script.js',
+  entry: './scripts/script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
