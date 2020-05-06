2795e4adef219d16e4252f72a312de162b4bd81c
leqsar <47753538+leqsar@users.noreply.github.com>
2020-05-06 14:45:49 +0300

    feat: install webpack and babel; feat: add basic layout

---
+++ movie-search/webpack.config.js
@@ -0,0 +1,21 @@
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+      rules: [
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: [
+              /node_modules/
+            ]
+          }
+      ]
+  }
+};
